2df2d68ea5e580e927eef69c4d7acf3c22c73a7d
edwardmccormick <ted.mccormick@gmail.com>
2021-02-09 12:32:18 -0600

    first constructor and menu

---
+++ src/Contact_Manage_Program.java
@@ -0,0 +1,12 @@
+public class Contact_Manage_Program {
+    public static void main(String[] args) {
+
+        System.out.println("1. View contacts." +
+        "2. Add a new contact." +
+        "3. Search a contact by name." +
+        "4. Delete an existing contact." +
+        "5. Exit.");
+        System.out.print("Enter an option (1, 2, 3, 4 or 5):");
+
+    }
+}
